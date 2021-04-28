b4b746d789fd683064b2b42d238a2353437844a5
mariaDulesova <maria.dulesova@gmail.com>
2021-04-28 16:23:52 +0200

    mail-reviewed

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -3,24 +3,29 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
 
+
+//Creo la lista con le mail
 var authorisedMailList = ["antonio.banderas@hotmail.com", "javier.bardem@gmail.com", "fernando.ray@yahoo.com", "luis.homar@hotmail.com", "alex.gonzales@yahoo.com"];
 
+//Chiedo al utente la sua mail
 var userMail = prompt("Please write your email:");
 
+// Controllo se la mail e' nella lista
+var authorisedMail = false; //di default la mail non e' autorizzata
 
 for (i = 0; i < authorisedMailList.length; i++) {
     if (userMail == authorisedMailList[i]) {
-        // console.log ("Congratulations, you are on the list!")
-        var message = "Congratulations, you are on the list!";
-        document.getElementById("result").innerHTML = message;
-    } else {
-        // console.log ("Unfortunately, you are not on the list!")
-        var message = "Unfortunately, you are not on the list!";
-        document.getElementById("result").innerHTML = message;
+        message = true; //se trovo la corrispondenza, la mail e' autorizzata
     }
 }
 
-console.log(message);
+//Stampo il messaggio a seconda del esito
+if (message == true) {
+    document.getElementById("result").innerHTML = "Congratulations, you are on the list!";
+} else {
+    document.getElementById("result").innerHTML = "Unfortunately, you are not on the list";
+}
+
 
 
 
