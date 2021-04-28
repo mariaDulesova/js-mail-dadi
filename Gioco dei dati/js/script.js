85867fdb559b23c34b9e3d3e2b543b67dec3d4c8
mariaDulesova <maria.dulesova@gmail.com>
2021-04-28 15:30:27 +0200

    2-gioco dei dadi completed

--- Gioco dei dati/js/script.js
+++ Gioco dei dati/js/script.js
@@ -3,10 +3,6 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
-//Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte?
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-
 
 var computer = Math.floor(Math.random() * 6) + 1;
 console.log("Score computer:", computer);
@@ -16,11 +12,16 @@ console.log("Score player:",player);
 
 if (computer>player) {
     var message ="Computer is the winner";
-    console.log(message);
+    // console.log(message);
+    document.getElementById("score").innerHTML=message;
 } else if( computer<player) {
     var message ="Player is the winner";
-    console.log(message);
+    // console.log(message);
+    document.getElementById("score").innerHTML=message;
 } else {
     var message ="You are even";
-    console.log(message);
+    // console.log(message);
+    document.getElementById("score").innerHTML=message;
 }
+
+console.log(message);
